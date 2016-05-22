d70295fbd1b1f56ac893abc9ee89b78d4def3907
mjibril <mjibril@users.noreply.github.com>
2016-05-22 22:40:50 +0100

    Initial commit

---
+++ angular_animate_css.js
@@ -0,0 +1,38 @@
+angular.module('animatecss.directive',[])
+    .directive('animateCss', function($timeout) {
+    return {
+
+	    link: function(scope, element, attr) {
+		var delay= attr.animateCssDelay || 800;
+		var delay= attr.animateCssDuration || null;
+		var css_class= attr.animateCss || 'flash';
+		var event= attr.animateCssEvent || null;
+		var prevent_default= attr.animateCssPrevent || true;
+		if(!event){
+		    
+		    $timeout(function(){
+			element.addClass("animated "+css_class);
+		    },delay);
+		 
+		}
+		else
+		{
+		    element.on(event, function(event) {
+			if(prevent_default)
+			{
+			    event.preventDefault();
+			}
+			element.addClass("animated "+css_class);
+			
+		    });
+
+		}
+		if(duration)
+		{
+		    $timeout(function(){
+			element.removeClass("animated "+css_class)
+		    },duration);
+		}
+	    }
+	};
+});
